8ca378afbf8a3f6944820b5c2b129783a5863de5
Sudheera Liyanage <sudheeral@gmail.com>
2022-06-27 16:07:25 +0530

    add directory list and item

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,14 @@
+import "./CategoryList.scss";
+import CategoryItem from "../CategoryItem/CategoryItem";
+
+const CategoryList = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
